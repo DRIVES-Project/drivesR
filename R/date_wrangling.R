f87efc90518ce0eb80b23bd65b2f35061fa6b974
= <=>
2024-08-01 16:46:58 -0400

    fixed issue with ISOdate (hopefully)

--- R/date_wrangling.R
+++ R/date_wrangling.R
@@ -63,13 +63,14 @@ identify_date_format <- function(date){
 #' It Also works with single digit month or day. 
 #' @param d a vector of dates in character format
 #'
-#' @return a vector of TRUE and FALSE indicating whether each date is in ISO format.
+#' @return a vector of TRUE and FALSE indicating whether each date is in ISO format. NAs are returns as NA.
 #' @export
 #'
 #' @examples
-#' myd = c("2024-01-01","2022-01-01","6/23/22","24-01-01")
+#' myd = c("2024-01-01","2022-01-01","6/23/22","24-01-01", NA,"jbs")
 #' is.ISOdate(myd)
 is.ISOdate <- function(d){
-  dvec <- as.character(as.Date(d))
-  return(!is.na(dvec) & substr(dvec,1,2)!="00")
+  dvec <- sapply(d, function(x){tryCatch(as.character(as.Date(x)), error = function(e)NA)})
+  outdvec <- ifelse(!is.na(d),!is.na(dvec) & substr(dvec,1,2)!="00",NA)
+  return(outdvec)
 }
