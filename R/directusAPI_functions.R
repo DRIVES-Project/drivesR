763e238def5a1505c0f5105dcf44eb14af8d44b4
= <=>
2025-01-14 11:51:51 -0500

    Removed for now. The API functions will take some troubleshooting.

--- R/directusAPI_functions.R
+++
@@ -1,8 +0,0 @@
-httr_dry_run <- function(r) {
-  require(httr)
-  old_cb <- httr::get_callback("request")
-  on.exit(httr::set_callback("request", old_cb))
-  
-  httr::set_callback("request", function(req) req)
-  r
-}
