2d6bf9e069bd5fd49fea7e49c5467eac32f7eb7c
= <=>
2025-02-04 12:29:14 -0500

    Trivial commit

--- R/date_wrangling.R
+++ R/date_wrangling.R
@@ -15,7 +15,7 @@
 #' myd = c("2024-01-01","2022/04/01","2023-15-01","6/23/22","24-01-01", NA,"jbs")
 #' cbind(myd, is.ISOdate(myd), is.ISOdate(myd, sep="-"))
 #' is.ISOdate(myd)
-is.ISOdate <- function(d,sepstr = NULL){
+is.ISOdate <- function(d, sepstr = NULL){
   ## checks if string is YYYY-MM-DD format 
   dvec <- sapply(d, function(x){tryCatch(as.character(as.Date(x)), error = function(e)NA)})
   ISOdatevec <- !is.na(dvec) & substr(dvec,1,2)!="00" # prevents 2-digit years from being interpreted as early history
