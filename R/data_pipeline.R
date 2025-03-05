25727b5eb4470ad9fd0fecfc80179cd5cd25864b
kemullerDRIVES <=>
2025-03-05 14:50:01 -0500

    Wrote a function to harmonize experimental treatment information.

--- R/data_pipeline.R
+++ R/data_pipeline.R
@@ -132,3 +132,76 @@ import_db_tables <- function(tablevec = NULL,
     return(db)
   }# ends if(save_locally == FALSE)
 }# ends function
+
+
+#' Harmonize treatment components
+#' 
+#' Combines information stored in the tables treatment_id_info and treatment_id_components into a time-series dataset
+#' describing combined experimental components for every year.
+#' @param db
+#' A list of database tables containing named dataframes treatment_id_info and treatment_id_components.
+#' If left NULL, these tables are imported from Directus.
+#'  
+#' @param mytoken 
+#' Directus API token, formatted as "Bearer myapitoken". Defaults to option set for "drivesR.default.directustoken"
+#' @returns
+#' A data frame with columns for site, year, treatmentID1, and treatmentID2 and all treatment types
+#' described in the table site_treatment_type_info
+#' @export
+#' @import dplyr
+#' @import tidyr
+#' @import purrr
+#' @examples
+#' #not run: tcw <- harmonize_treatments()
+harmonize_treatments <- function(db = NULL, 
+                                 mytoken = getOption("drivesR.default.directustoken")){
+  # if db is supplied, check for required tables. 
+  trttables <- c("treatment_id_info","treatment_id_components")
+  if(!is.null(db)){
+    if(any(!trttables %in% names(db))){
+      stop("List supplied to db must contain data frames named treatment_id_info and treatment_id_components")
+    }
+  }
+    if(is.null(db)){
+    # Import from directus
+      db <- import_db_tables(tablevec = c(trttables), mytoken = mytoken,save_locally = FALSE, import_from_local = FALSE)
+  }
+  # add treatment_id_info to components
+  treatment_id_components <- dplyr::left_join(db$treatment_id_components, db$treatment_id_info)
+  # make a wide version, separating treatment types 1 and 2. 
+  tc1 <- treatment_id_components[which(treatment_id_components$treatment_id_type=="treatmentID1"),] %>% expand_years()
+  tc2 <- treatment_id_components[which(treatment_id_components$treatment_id_type=="treatmentID2"),] %>% expand_years()
+  tc1w <- dplyr::select(tc1,-uid,-treatment_level_uid,-note,-treatment_id_type) %>% 
+    tidyr::pivot_wider(id_cols = c("site_id","treatment_id","year"),
+                names_from = "treatment_type",
+                values_from = "treatment_level") %>%
+    rename("treatmentID1" = 'treatment_id')
+  tc2w <- dplyr::select(tc2,-uid,-treatment_level_uid,-note,-treatment_id_type) %>% 
+    tidyr::pivot_wider(id_cols = c("site_id","treatment_id","year"),
+                names_from = "treatment_type",
+                values_from = "treatment_level") %>%
+    dplyr::rename("treatmentID2" = 'treatment_id')
+  
+  # step 1: merge wide yearly treatment components.
+  mergenames <- c("site_id","treatmentID1","year")## cols to merge on
+  step1names <- c("treatmentID2","entryPhase") # cols added in step 1
+  step2names <- setdiff(names(tc2w), c(mergenames,step1names)) # cols overwritten in step 2.
+  tcw <- dplyr::left_join(tc2w, tc1w, by = mergenames, suffix = c(".t2",".t1")) %>% as.data.frame()
+  ## overlapping treatment components will be given .t2 from tc2 and .t1 from tc1
+  ## step 2: overwrite any columns specified differently in components of treatmentID2 
+  # (usually due to discrepencies in rotation sequences across phases)
+  for(i in seq_along(step2names)){
+    # note: tcw must be a dataframe for this to work (not a tibble)
+    t2name <- paste0(step2names[i],".t2")
+    t1name <- paste0(step2names[i],".t1")
+    tcw[,step2names[i]]<- ifelse(is.na(tcw[,t2name]),tcw[,t1name],tcw[,t2name])
+  }
+  # remove duplicated treatment component columns. 
+  dupnames <- purrr::map(step2names, ~paste0(.x, c(".t1",".t2"))) %>% unlist()
+  tcw <- tcw %>% dplyr::select(!all_of(dupnames))
+  # relocate some columns
+  tcw <- tcw %>% dplyr::relocate(treatmentID1, .before = year) %>%
+    dplyr::relocate(rotation_id, .before = entryPhase)
+  
+  return(tcw)   
+}# ends function
