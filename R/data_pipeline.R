082d3c882c1cfeef29f7add24aaa8930bf713977
kemullerDRIVES <=>
2025-03-05 16:34:10 -0500

    Wrote a function to harmonize treatment information for experimental units.

--- R/data_pipeline.R
+++ R/data_pipeline.R
@@ -205,3 +205,48 @@ harmonize_treatments <- function(db = NULL,
   
   return(tcw)   
 }# ends function
+
+
+#' Harmonize treatment and unit information
+#' 
+#' Combines information from database tables to describe which treatment components
+#' were applied to which experimental units in which years. 
+#' @param db 
+#' A list of database tables containing named dataframes treatment_id_info,
+#' treatment_id_components, and experimental_unit_treatments.
+#' If left NULL, these tables are imported from Directus.
+#' @param mytoken 
+#' Directus token, formatted as "Bearer myapitoken". Can be set with set_default_token()
+#' @returns
+#' A data frame with one row per unit ID per year and all treatment components in columns. 
+#' @export
+#' @import tidyr
+#' @import dplyr
+#' @examples
+#' # not run: trt_units <- harmonize_treatment_units()
+harmonize_treatments_units <- function(db = NULL,
+                                       mytoken = getOption("drivesR.default.directustoken")){
+  trttables <- c("treatment_id_info","treatment_id_components","experimental_unit_treatments")
+  if(!is.null(db)){
+    if(any(!trttables %in% names(db))){
+      stop("List supplied to db must contain data frames named treatment_id_info,
+           treatment_id_components, and experimental_unit_treatments.")
+    }
+  }
+  if(is.null(db)){
+    db <- import_db_tables(trttables, mytoken = mytoken,save_locally = FALSE, import_from_local = FALSE)
+  }
+  ## get harmonized treatments.
+  tcw <- harmonize_treatments(db = db)
+  ## add treatment_id info
+  experimental_unit_treatments <- dplyr::left_join(db$experimental_unit_treatments, db$treatment_id_info)
+  ## expand to a yearly time series and put treatment types in separate columns. 
+  yearly_unit_trt <- expand_years(experimental_unit_treatments) %>%
+    tidyr::pivot_wider(id_cols = c('site_id',"unit_id","year"), 
+                names_from = "treatment_id_type",
+                values_from = "treatment_id" )
+  # merge yearly treatment components with yearly_unit_trt.
+  yearly_unit_trt2 <- left_join(yearly_unit_trt, subset(tcw, select = -c(treatmentID1)),
+                                by = c("site_id", "treatmentID2", "year"))
+  return(yearly_unit_trt2) 
+}
