1ac9eb9c311a4c3be75637492dc5b416e8ecacc8
kemullerDRIVES <=>
2025-03-13 18:31:45 -0400

    Made function harmonize_yields_treatments that combines yields with treatment info. Also updated documentation for harmonize_yields.

--- R/data_pipeline.R
+++ R/data_pipeline.R
@@ -297,7 +297,9 @@ list_treatments_by_management_practice <- function(site_treatment_type_info = NU
 #' @param mytoken 
 #' Directus token, formatted as "Bearer myapitoken". Can be set with set_default_token().
 #' @param crop_fractions_as_columns
-#' TRUE or FALSE for whether the ...
+#' If TRUE, multiple fractions from the same crop are organized in separate 
+#' columns. If FALSE, multiple fractions from the same crop are organized in separate rows, 
+#' as in the database table.
 #' @param primary_crop_fractions
 #' A vector of crop fractions to select as the primary fraction, when there is more than one.
 #' So far, this only pertains to grain and tomato fruit.
@@ -351,3 +353,47 @@ harmonize_yields <- function(crop_yields = NULL,
       
   }
 }
+
+#' Harmonize yield data with experimental treatment data.
+#'
+#' @param db 
+#' #' A list of database tables containing named dataframes treatment_id_info,
+#'  treatment_id_components, experimental_unit_treatments, and crop_yields.
+#' If left NULL, these tables are imported from Directus.
+#' @param crop_fractions_as_columns
+#' #' If TRUE, multiple fractions from the same crop are organized in separate 
+#' columns. If FALSE, multiple fractions from the same crop are organized in separate rows, 
+#' as in the database table. 
+#' @param mytoken 
+#' Directus token, formatted as "Bearer myapitoken". Set by set_default_token().
+#' @returns
+#' A dataframe of yield data combined with experimental treatment data, with each treatment
+#' type in a separate column. 
+#' If crop_fractions_as_columns is set to TRUE, 
+#' data describing fractions from the same crop will be described in separate columns, with 
+#' suffix _1 for the primary fraction and _2, _3, etc. for other fractions. 
+#' @export
+#' @import dplyr
+#' @examples
+#'  # not run: longyield <- harmonize_yields_treatments(crop_fractions_as_columns = FALSE)
+#' # not run: wideyield <- harmonize_yields(crop_fractions_as_columns = TRUE)
+harmonize_yields_treatments <- function(
+    db = NULL,
+    crop_fractions_as_columns = FALSE,
+    mytoken = getOption("drivesR.default.directustoken")){
+  ytrttables <- c("treatment_id_info","treatment_id_components","experimental_unit_treatments","crop_yields")
+  if(!is.null(db)){
+    if(any(!ytrttables %in% names(db))){
+      stop(paste0("List supplied to db must contain data frames named ",paste(ytrttables, collapse=", ") ))
+    }
+  }
+  if(is.null(db)){
+    db <- import_db_tables(ytrttables, mytoken = mytoken,save_locally = FALSE, import_from_local = FALSE)
+  }
+  treatmentunits <- harmonize_treatments_units(db = db)
+  yields <- harmonize_yields(crop_yields = db$crop_yields, 
+                             crop_fractions_as_columns = crop_fractions_as_columns,
+                             mytoken = mytoken)
+  outyield <- dplyr::left_join(yields, treatmentunits, by = c("site_id","unit_id","harvest_year"="year"))
+  return(outyield)
+  }
