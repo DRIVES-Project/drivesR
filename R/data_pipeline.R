8696a60be6d4d3283a21697e4531918049d5fe5b
kemullerDRIVES <=>
2025-03-20 18:01:27 -0400

    Finished function to harmonize harvest dates (it was a head scratcher).

--- R/data_pipeline.R
+++ R/data_pipeline.R
@@ -473,26 +473,69 @@ harmonize_weather <- function(weather_daily=NULL,
 #' @param harvest_dates 
 #' A data frame of the harvest_dates table from the DRIVES database. 
 #' If NULL, this table is downloaded from Directus.
-#' @param mytoken
-#' Directus API token, formatted as "Bearer apitoken". Can be set with set_default_token() 
 #' @param public 
 #' TRUE if data are to be downloaded from the publicly available part of the DRIVES 
 #' database. FALSE otherwise. 
+#' @param crop_fractions_as_columns
+#' If TRUE, multiple fractions from the same crop are organized in separate 
+#' columns. If FALSE, multiple fractions from the same crop are organized in separate rows, 
+#' as in the database table. Although multiple crop fractions are rare, this option
+#' helps to merge with the yield data.
+#' @param mytoken
+#' Directus API token, formatted as "Bearer apitoken". Can be set with set_default_token() 
+#' @param primary_crop_fractions
+#' A vector of crop fractions to select as the primary fraction, when there is more than one.
+#' So far, this only pertains to grain and tomato fruit. The default is set with the 
+#' option drivesR.primary_crop_fractions. 
 #' @returns
-#' A data frame of harvest date data with minor changes.
-#' ...
+#' A data frame of harvest date data with minor changes with one row per unit/year/crop.
+#' Multiple harvests are separated into columns. Multiple 
 #' @export
-#'
+#' @import dplyr
 #' @examples
+#' # not run: harv1 <- harmonize_harvest_dates(crop_fractions_as_columns = TRUE)
+#' # not run: harv2 <- harmonize_harvest_dates(crop_fractions_as_columns = FALSE)
 harmonize_harvest_dates <- function(harvest_dates = NULL,
-                                    mytoken = getOption("drivesR.default.directustoken"),
-                                    public = FALSE){
+                                    public = FALSE,
+                                    crop_fractions_as_columns = FALSE,
+                                    primary_crop_fractions = getOption("drivesR.primary_crop_fractions"),
+                                    mytoken = getOption("drivesR.default.directustoken")
+                                    ){
   if(is.null(harvest_dates)){
     tablename <- ifelse(public == TRUE,"public_harvest_dates","harvest_dates")
     harvest_dates <- get_db_table(tablename, mytoken = mytoken)
   }
- ## Step 1: fill in missing actual_crop_id with expected_crop_id
+ ## Step 1: fill in missing actual_crop_id with expected_crop_id----
+  # fill in missing crop fractions with 'none'
   nacrop <- which(is.na(harvest_dates$actual_crop_id))
   harvest_dates$actual_crop_id[nacrop] <- harvest_dates$expected_crop_id[nacrop]
-  
+  nafrac <- which(is.na(harvest_dates$harvested_fraction))
+  harvest_dates$harvested_fraction[nafrac] <- "none"
+ 
+  ## Step 2: if crop fractions are separated into columns-----
+  if(crop_fractions_as_columns== TRUE){
+    # add fraction_index (similar to harmonize_crop_yields)
+    harvest_dates <- harvest_dates %>% group_by(site_id,unit_id,actual_crop_id,harvest_year) %>%
+      mutate(num_fractions = length(unique(harvested_fraction)))
+    
+    cond1 <- harvest_dates$num_fractions==1 | harvest_dates$harvested_fraction %in% primary_crop_fractions
+    harvest_dates$primary_fraction <- cond1
+    harvest_dates <- harvest_dates %>% group_by(site_id, actual_crop_id,unit_id, harvest_year) %>%
+      mutate(fraction_index = ifelse(primary_fraction,1,2:length(unique(harvested_fraction))))
+    
+    # adjust harvest number using fraction index.
+    harvest_dates$harvest_number <- harvest_dates$harvest_number + harvest_dates$fraction_index-1
+    ## remove columns I added. 
+    harvest_dates <- select(harvest_dates, -num_fractions,-primary_fraction,-fraction_index)
+    ## set up columns that should be pivoted to wide
+    valcols = c("harvest_date","harvested_fraction","harvest_notes","uid")
+  }else{
+    valcols = c("harvest_date","harvest_notes","uid")
+  }
+  ## Step 3: pivot to wide ------
+  namecols = c("harvest_number")
+  wide_harvest <- tidyr::pivot_wider(harvest_dates, 
+                              names_from = all_of(namecols),
+                              values_from = all_of(valcols))
+  return(wide_harvest)
 }
