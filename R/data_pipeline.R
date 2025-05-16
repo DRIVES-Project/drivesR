67252d48daf17c08f726897b57b82a85dbf4e859
kemullerDRIVES <=>
2025-05-16 15:29:40 -0400

    fixed a minor bug in list_treatments_by_management_practice and updated the documentation

--- R/data_pipeline.R
+++ R/data_pipeline.R
@@ -349,9 +349,14 @@ harmonize_treatments_units <- function(db = NULL){
 #' List treatment components by management practice.
 #' 
 #' Uses information in the site_treatment_type_info table 
-#' @param site_treatment_type_info 
-#' A data frame with the site_treatment_type_info table. If left as
-#' NULL, this table is imported from directus. 
+#' @param db 
+#' A named list of data frames containing the site_treatment_type_info
+#' and treatment_id_components table. If left as
+#' NULL, these tables are imported from directus. 
+#' @param match_trt
+#' Specifies whether to remove treatment types that are not 
+#' referenced in the treatment_id_components table. Defaults to TRUE,
+#' with a warning mentioning which are removed.
 #' @returns
 #' A named list of treatment types corresponding to a set of core management practices described for
 #' all sites. Useful for identifying columns in harmonized dataframes with all treatment types. 
@@ -367,9 +372,27 @@ harmonize_treatments_units <- function(db = NULL){
 #' #not run: nfert <- harmonized_treatments[,c("site_id",
 #'                                            #"treatmentID2","year"
 #'                                           #,management_practice_list$`N fertility)]
-list_treatments_by_management_practice <- function(site_treatment_type_info = NULL){
-  if(is.null(site_treatment_type_info)){
-    site_treatment_type_info <- get_db_table("site_treatment_type_info")
+list_treatments_by_management_practice <- function(db = NULL,
+                                                   match_trt = TRUE){
+  
+  dbtables <- c("site_treatment_type_info","treatment_id_components")
+  if(!is.null(db)){
+    if(any(!dbtables %in% names(db))){
+      stop("List supplied to db must contain data frames named site_treatment_type_info and treatment_id_components.")
+    }
+  }
+  if(is.null(db)){
+    db <- import_db_tables(dbtables,fetch_option = "download.only")
+  }
+  typesToRemove <- setdiff(db$site_treatment_type_info$treatment_type,
+                           db$treatment_id_components$treatment_type)
+  if(length(typesToRemove) > 0 & match_trt == TRUE ){
+    site_treatment_type_info <- 
+      db$site_treatment_type_info[which(!db$site_treatment_type_info$treatment_type %in% typesToRemove),]
+    warning("Removed ", length(typesToRemove)," treatment types not matched to a treatment id: ",
+            paste(typesToRemove, collapse=", "))
+  }else{
+    site_treatment_type_info <- db$site_treatment_type_info
   }
   management_practice_list <- tapply(site_treatment_type_info$treatment_type,
                                      site_treatment_type_info$management_practice, unique)
